d176f65653a76c6648bd69ea45afb78755a270e3
SEANSLAPTOP\Shizhen Li <shizhen.li@uwaterloo.ca>
2020-01-13 16:48:51 -0500

    a small addition

---
+++ wordGenerator.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+int main(){
+    int wordSize{};
+    cout<<"How long is the word you are trying to generate?"<<endl;
+    cin>>wordSize;
+}
